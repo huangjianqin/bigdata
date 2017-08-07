d185bc1b09952671fc100c8163838a2f971957fb
huangjianqin <18814127639@163.com>
2017-08-07 09:24:31 +0800

    spark hive结合demo

---
+++ src/test/scala/org/kin/bigdata/hive/HiveContextTest.scala
@@ -0,0 +1,20 @@
+package org.kin.bigdata.hive
+
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * Created by 健勤 on 2017/8/6.
+ */
+object HiveContextTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("HiveContextTest").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val hiveContext = new HiveContext(sc)
+
+    val movies = hiveContext.sql("select * from movie_info")
+    movies.foreach(println)
+
+    sc.stop()
+  }
+}
