ba2e8b8bdfe41b6568f4fcadc24531aadd694083
huangjianqin <18814127639@163.com>
2018-01-26 09:59:26 +0800

    优化代码,修改部分逻辑

--- Kin-Framework/src/main/java/org/kin/framework/event/EventHandler.java
+++ Kin-Framework/src/main/java/org/kin/framework/event/EventHandler.java
@@ -4,6 +4,7 @@ package org.kin.framework.event;
  * Created by 健勤 on 2017/8/8.
  * 事件处理器接口
  */
+@FunctionalInterface
 public interface EventHandler<T extends Event> {
     void handle(T event);
 }
--- Kin-Framework/src/main/java/org/kin/framework/statemachine/SingleArcTransition.java
+++ Kin-Framework/src/main/java/org/kin/framework/statemachine/SingleArcTransition.java
@@ -4,6 +4,7 @@ package org.kin.framework.statemachine;
  * Created by 健勤 on 2017/8/9.
  * 一对一状态转换逻辑处理
  */
+@FunctionalInterface
 public interface SingleArcTransition<OPERAND, EVENT> {
     void transition(OPERAND operand, EVENT event);
 }
