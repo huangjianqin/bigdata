2274d76d39dbf808b65dae89e3c97f11eec6e46a
huangjianqin <18814127639@163.com>
2018-05-26 16:21:27 +0800

    LRU map

--- Kin-Framework/src/main/java/org/kin/framework/collection/LRUMap.java
+++ Kin-Framework/src/main/java/org/kin/framework/collection/LRUMap.java
@@ -1,19 +1,27 @@
 package org.kin.framework.collection;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by huangjianqin on 2017/10/28.
  */
 public class LRUMap<K, V> extends LinkedHashMap<K, V> {
     private int initItemNum;
+    private final int _maxItemNum;
 
-    public LRUMap(){
-        super(1000, 0.8f, true);
+    public LRUMap(int maxItemNum){
+        this(2, maxItemNum);
     }
 
-    public LRUMap(int initItemNum) {
+    public LRUMap(int initItemNum, int maxItemNum) {
         super(initItemNum, 0.8f, true);
         this.initItemNum = initItemNum;
+        this._maxItemNum = maxItemNum;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > _maxItemNum;
     }
 }
