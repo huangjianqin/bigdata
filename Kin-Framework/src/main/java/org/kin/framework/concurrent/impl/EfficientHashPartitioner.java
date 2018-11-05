84d21a5a9cb4c64b557b018e62f76acbb1bb300d
huangjianqin <18814127639@163.com>
2018-11-05 11:06:01 +0800

    更高效的Hash 分区Partitioner

---
+++ Kin-Framework/src/main/java/org/kin/framework/concurrent/impl/EfficientHashPartitioner.java
@@ -0,0 +1,16 @@
+package org.kin.framework.concurrent.impl;
+
+import org.kin.framework.concurrent.Partitioner;
+
+/**
+ * Created by huangjianqin on 2018/11/5.
+ * HashMap的Hash方式
+ * 更高效的hash方式
+ */
+public class EfficientHashPartitioner<K> implements Partitioner<K> {
+    public int toPartition(K key, int numPartition) {
+        int h;
+        //高低位异或 目的是增加hash的复杂度
+        return key == null ? 0 : (((h = key.hashCode()) ^ h >>> 16) & (numPartition - 1));
+    }
+}
--- Kin-Framework/src/main/java/org/kin/framework/concurrent/impl/HashPartitioner.java
+++ Kin-Framework/src/main/java/org/kin/framework/concurrent/impl/HashPartitioner.java
@@ -4,9 +4,10 @@ import org.kin.framework.concurrent.Partitioner;
 
 /**
  * Created by huangjianqin on 2017/10/26.
+ * HashTable的Hash方式
  */
 public class HashPartitioner<K> implements Partitioner<K> {
     public int toPartition(K key, int numPartition) {
-        return (key.hashCode() & Integer.MAX_VALUE) % numPartition;
+        return key == null ? 0 : ((key.hashCode() & Integer.MAX_VALUE) % numPartition);
     }
 }
